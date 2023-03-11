c85571cbb7bc3a615ae0f8ca081d8b69dd5b142e
Ahmet Caliskan <root@systemalert.org>
2023-03-11 03:57:41 +0100

    fix paths in conftest

--- imgp/tests/conftest.py
+++ imgp/tests/conftest.py
@@ -11,21 +11,13 @@ current_path = os.getcwd()
 @pytest.fixture()
 def img_paths():
     return [
-        os.path.join(root, "..", "tests", "images", "cats", "cat-gfac16a4e0_640.jpg"),
-        os.path.join(root, "..", "tests", "images", "cats", "cat-ga7f8f27f4_640.jpg"),
-        os.path.join(root, "..", "tests", "images", "cats", "cat-g9f0ac3883_640.jpg"),
-        os.path.join(root, "..", "tests", "images", "frogs", "frog-g9c52de5c6_640.jpg"),
-        os.path.join(
-            root, "..", "tests", "images", "frogs", "waters-g38a3c5715_640.jpg"
-        ),
-        os.path.join(root, "..", "tests", "images", "frogs", "frog-gdc7a17746_640.jpg"),
-        os.path.join(
-            root, "..", "tests", "images", "giraffes", "giraffe-ge696dba15_640.jpg"
-        ),
-        os.path.join(
-            root, "..", "tests", "images", "giraffes", "giraffe-g2122926f3_640.jpg"
-        ),
-        os.path.join(
-            root, "..", "tests", "images", "giraffes", "giraffe-g9b3e5ee1f_640.jpg"
-        ),
+        os.path.join(root, "tests", "images", "cats", "cat-gfac16a4e0_640.jpg"),
+        os.path.join(root, "tests", "images", "cats", "cat-ga7f8f27f4_640.jpg"),
+        os.path.join(root, "tests", "images", "cats", "cat-g9f0ac3883_640.jpg"),
+        os.path.join(root, "tests", "images", "frogs", "frog-g9c52de5c6_640.jpg"),
+        os.path.join(root, "tests", "images", "frogs", "waters-g38a3c5715_640.jpg"),
+        os.path.join(root, "tests", "images", "frogs", "frog-gdc7a17746_640.jpg"),
+        os.path.join(root, "tests", "images", "giraffes", "giraffe-ge696dba15_640.jpg"),
+        os.path.join(root, "tests", "images", "giraffes", "giraffe-g2122926f3_640.jpg"),
+        os.path.join(root, "tests", "images", "giraffes", "giraffe-g9b3e5ee1f_640.jpg"),
     ]
