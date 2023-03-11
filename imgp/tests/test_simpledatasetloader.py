c1a0a85add0bd67d204ea7fea0bee3c5483fa78b
Ahmet Caliskan <root@systemalert.org>
2023-03-11 05:36:27 +0100

    bunch of more tests for datasetloader

--- imgp/tests/test_simpledatasetloader.py
+++ imgp/tests/test_simpledatasetloader.py
@@ -2,11 +2,12 @@ from imgp.datasets.simpledatasetloader import SimpleDatasetLoader
 from imgp.preprocessing.simpleprocessor import SimplePreprocessor
 import numpy.testing as npt
 import numpy as np
+import pytest
 
 
 def test_load(img_paths):
     sp = SimplePreprocessor(32, 32)
-    dl = SimpleDatasetLoader([sp])
+    dl = SimpleDatasetLoader(preprocessors=[sp])
     data, labels = dl.load(img_paths)
     expected = np.array(
         [
@@ -21,7 +22,21 @@ def test_load(img_paths):
             "giraffes",
         ]
     )
+    assert isinstance(data, np.ndarray)
+    assert isinstance(labels, np.ndarray)
     assert data.shape == (9, 32, 32, 3)
     assert labels.shape == (9,)
     assert len(labels) == len(expected)
     assert list(labels) == list(expected)
+
+
+def test_load_without_preprocessors(img_paths):
+    dl = SimpleDatasetLoader(preprocessors=None)
+    with pytest.raises(Exception) as e_info:
+        dl.load(img_paths)
+
+
+def test_load_verbose(img_paths):
+    dl = SimpleDatasetLoader()
+    with pytest.raises(Exception) as e_info:
+        dl.load(img_paths, 1)
