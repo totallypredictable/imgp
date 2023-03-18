4cac3737e3e60d15fe8c2f61252146618bf84cf5
Ahmet Caliskan <root@systemalert.org>
2023-03-19 00:22:05 +0100

    FIX preprocessing init

--- imgp/preprocessing/__init__.py
+++ imgp/preprocessing/__init__.py
@@ -1,4 +1,4 @@
 from .simpleprocessor import SimplePreprocessor
 from .imagetoarraypreprocessor import ImageToArrayPreprocessor
 
-__all__ = ["simpleprocessor", "imagetoarraypreprocessor"]
+__all__ = ["SimplePreprocessor", "ImageToArrayPreprocessor"]
