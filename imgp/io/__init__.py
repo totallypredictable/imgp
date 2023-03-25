8b264193ec3b3cdd7f2b5b9d28ed2104fd5a56e7
Ahmet Caliskan <root@systemalert.org>
2023-03-25 16:57:46 +0100

    ADD HDF5DatasetGenerator

--- imgp/io/__init__.py
+++ imgp/io/__init__.py
@@ -1,3 +1,7 @@
 from .hdf5datasetwriter import HDF5DatasetWriter
+from .hdf5datasetgenerator import HDF5DatasetGenerator
 
-__all__ = ["HDF5DatasetWriter"]
+__all__ = [
+    "HDF5DatasetWriter",
+    "HDF5DatasetGenerator",
+]
